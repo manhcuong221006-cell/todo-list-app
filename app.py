41c54ca66fab88dcb09ed52b0d40cb50cc2fba02
Mạnh Cường <manhcuong221006@gmail.com>
2025-10-18 22:51:15 +0700

    feat: Implement function to add a new task

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ tasks = []
 def add_task(task_name):
     """Thêm một công việc mới vào danh sách."""
     tasks.append(task_name)
-    printf(f"Đã thêm công việc: '{task_name}'")
-    # ---Điểm bắt đầu của chương trình---
-    if __name__ == "__main__":
-        print("Chào mừng đến với ứng dụng To-Do List!")
-        add_task("Học bài Git và Github")
-        add_task("Làm bài tập thực hành ở nhà")
+    print(f"Đã thêm công việc: '{task_name}'")
+# ---Điểm bắt đầu của chương trình---
+if __name__ == "__main__":
+    print("Chào mừng đến với ứng dụng To-Do List!")
+    add_task("Học bài Git và Github")
+    add_task("Làm bài tập thực hành ở nhà")
+
+def list_tasks(task_name):
+    for i in range(len(task_name)):
+        print(f"{i + 1}. {task_name[i]}")
